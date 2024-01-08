86f2a65830a33b7b55d1189e8309949884057afe
Oscar Eriksson <oscar@thenetcircle.com>
2024-01-08 23:16:32 +0800

    day11 tests done

--- 2023/day11.py
+++ 2023/day11.py
@@ -1,5 +1,6 @@
 import math
 import sys
+import itertools
 from pprint import pprint
 
 
@@ -12,9 +13,6 @@ def process_part_1(file_name):
 
     expanded_cols = [""] * len(chart)
     expanded_chart = list()
-
-    for line in chart:
-        print(line)
 
     for row_nr, row in enumerate(chart):
         if all((c == '.' for c in row)):
@@ -30,20 +28,30 @@ def process_part_1(file_name):
         if all_empty:
             cols_to_expand.append(col_nr)
 
+    # expand column wise first (strings)
     for row_nr, row in enumerate(chart):
         for col_nr in range(len(row)):
             expanded_cols[row_nr] += chart[row_nr][col_nr]
             if col_nr in cols_to_expand:
                 expanded_cols[row_nr] += chart[row_nr][col_nr]
 
+    # then expand the rows (list)
     for row_nr, row in enumerate(expanded_cols):
         expanded_chart.append(row)
         if row_nr in rows_to_expand:
             expanded_chart.append(row)
 
-    print()
-    for line in expanded_chart:
-        print(line)
+    galaxies = list()
+    for row_nr, row in enumerate(expanded_chart):
+        for col_nr, col in enumerate(row):
+            if col == '#':
+                galaxies.append((row_nr, col_nr))
+
+    all_distances = 0
+    for galaxy_a, galaxy_b in itertools.combinations(galaxies, 2):
+        all_distances += abs(galaxy_b[0] - galaxy_a[0]) + abs(galaxy_b[1] - galaxy_a[1])
+
+    return all_distances
 
 
 def process_part_2(file_name):
@@ -61,7 +69,7 @@ def test_part_1():
 
 
 def test_part_2():
-    return_value = process_part_2('day9_tests.txt')
+    return_value = process_part_2('day11_tests.txt')
     if return_value != 2:
         print(f'[test part2] expected 2 got {return_value}')
         sys.exit(1)
@@ -70,12 +78,12 @@ def test_part_2():
 
 
 def part_1():
-    result = process_part_1('day10.txt')
+    result = process_part_1('day11.txt')
     print(f"[part1] {result}")
 
 
 def part_2():
-    result = process_part_2('day10.txt')
+    result = process_part_2('day11.txt')
     print(f"[part2] {result}")
 
 
